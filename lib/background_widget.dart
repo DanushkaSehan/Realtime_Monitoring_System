a620ea90ad76942dc18179e6ea0613aac1ad5b00
adeesha-riv <39-bce-0005@kdu.ac.lk>
2023-09-10 18:21:02 +0530

    background of homescreen and add fabric details

---
+++ lib/background_widget.dart
@@ -0,0 +1,163 @@
+import 'package:flutter/material.dart';
+
+class BackgroundWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ShaderMask(
+      shaderCallback: (Rect bounds) {
+        return LinearGradient(
+          colors: [
+            const Color.fromARGB(
+                255, 122, 188, 221), // Color for the first wave
+            Colors.blueAccent, // Color for the second wave
+            Color.fromARGB(255, 132, 53, 174), // Color for the third wave
+          ],
+          stops: [0.0, 0.5, 1.0],
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+        ).createShader(bounds);
+      },
+      blendMode: BlendMode.srcATop,
+      child: Stack(
+        children: [
+          CustomPaint(
+            painter: _WaveBackgroundPainter(150, true, 0.2),
+            child: SizedBox.expand(),
+          ),
+          CustomPaint(
+            painter: _WaveBackgroundPainter(150, false, 0.4),
+            child: SizedBox.expand(),
+          ),
+          CustomPaint(
+            painter: _WaveBackgroundPainter(150, true, 0.6),
+            child: SizedBox.expand(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _WaveBackgroundPainter extends CustomPainter {
+  final double waveHeight;
+  final bool isTopRight;
+  final double opacity;
+
+  _WaveBackgroundPainter(this.waveHeight, this.isTopRight, this.opacity);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    final Paint paint = Paint();
+    paint.color = Colors.white.withOpacity(opacity);
+
+    final Path path = Path();
+    path.lineTo(size.width, 0);
+
+    if (isTopRight) {
+      path.cubicTo(
+        size.width * 0.7,
+        waveHeight * 0.6,
+        size.width * 0.4,
+        waveHeight * 0.4,
+        size.width,
+        waveHeight,
+      );
+    } else {
+      path.cubicTo(
+        size.width * 0.7,
+        size.height - waveHeight * 0.6,
+        size.width * 0.4,
+        size.height - waveHeight * 0.4,
+        size.width,
+        size.height - waveHeight,
+      );
+    }
+
+    path.lineTo(size.width, size.height);
+    path.lineTo(0, size.height);
+
+    if (isTopRight) {
+      path.cubicTo(
+        size.width * 0.3,
+        waveHeight * 1.4,
+        size.width * 0.2,
+        waveHeight * 1.2,
+        0,
+        waveHeight,
+      );
+    } else {
+      path.cubicTo(
+        size.width * 0.3,
+        size.height - waveHeight * 1.4,
+        size.width * 0.2,
+        size.height - waveHeight * 1.2,
+        0,
+        size.height - waveHeight,
+      );
+    }
+
+    canvas.drawPath(path, paint);
+  }
+
+  @override
+  bool shouldRepaint(covariant CustomPainter oldDelegate) {
+    return false;
+  }
+}
+
+
+
+
+// import 'package:flutter/material.dart';
+
+// class BackgroundWidget extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return CustomPaint(
+//       painter: _WaveBackgroundPainter(),
+//       child: Container(),
+//     );
+//   }
+// }
+
+// class _WaveBackgroundPainter extends CustomPainter {
+//   @override
+//   void paint(Canvas canvas, Size size) {
+//     final Paint paint = Paint();
+//     paint.color = const Color.fromARGB(255, 18, 19, 26);
+
+//     const double waveHeight = 150;
+//     final topPartHeight = size.height * 0.25;
+
+//     final Path path = Path();
+//     path.lineTo(size.width, 0);
+//     path.lineTo(size.width, topPartHeight);
+//     path.cubicTo(
+//       size.width,
+//       topPartHeight + waveHeight * 0.39,
+//       size.width * 0.84,
+//       topPartHeight + waveHeight * 0.86,
+//       size.width / 2,
+//       topPartHeight + waveHeight * 0.6,
+//     );
+//     path.cubicTo(
+//       size.width * 0.16,
+//       topPartHeight + waveHeight * 0.35,
+//       size.width * 0.02,
+//       topPartHeight + waveHeight * 0.85,
+//       0,
+//       topPartHeight + waveHeight,
+//     );
+//     path.lineTo(0, size.height);
+
+//     canvas.drawPath(
+//       path,
+//       paint,
+//     );
+//   }
+
+//   @override
+//   bool shouldRepaint(covariant CustomPainter oldDelegate) {
+//     return false;
+//   }
+// }
--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,6 +6,7 @@ import 'data_display.dart'; // Import your real-time data screen
 import 'maintenance_screen.dart';
 import 'reports_screen.dart';
 import 'signin_screen.dart';
+import 'background_widget.dart'; // Import the BackgroundWidget
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,6 +17,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   final String currentUserUID = FirebaseAuth.instance.currentUser?.uid ?? "";
+
   @override
   Widget build(BuildContext context) {
     if (currentUserUID == "v7GgJmld33eU5K16QSBF7kgFIK23") {
@@ -23,7 +25,6 @@ class _HomeScreenState extends State<HomeScreen> {
       return const MaintenanceScreen();
     } else {
       return Scaffold(
-        //backgroundColor: Color.fromARGB(255, 18, 19, 26),
         appBar: AppBar(
           actions: <Widget>[
             IconButton(
@@ -34,8 +35,12 @@ class _HomeScreenState extends State<HomeScreen> {
               onPressed: () {
                 FirebaseAuth.instance.signOut().then((value) {
                   print("Signed Out");
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => const SignInScreen()));
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const SignInScreen(),
+                    ),
+                  );
                 });
               },
             )
@@ -53,78 +58,240 @@ class _HomeScreenState extends State<HomeScreen> {
             height: 90,
           ),
         ),
-        body: Center(
-          child: Column(
-            children: [
-              const SizedBox(height: 150),
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(200, 80),
-                    backgroundColor: const Color.fromARGB(255, 35, 28, 75),
-                    shape: const ContinuousRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)))),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const FabricScreen()),
-                  );
-                },
-                icon: const Icon(Icons.add),
-                label: const Text('Add Fabric Details'),
+        body: Stack(
+          children: [
+            BackgroundWidget(), // Add the BackgroundWidget here
+            Center(
+              child: Column(
+                children: [
+                  const SizedBox(height: 150),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(200, 80),
+                      backgroundColor: const Color.fromARGB(255, 35, 28, 75),
+                      shape: const ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const FabricScreen(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.add),
+                    label: const Text('Add Fabric Details'),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(200, 80),
+                      backgroundColor: const Color.fromARGB(255, 35, 28, 75),
+                      shape: const ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const DataScreen(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.account_tree_rounded),
+                    label: const Text('Real Time Tracking'),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(200, 80),
+                      backgroundColor: const Color.fromARGB(255, 35, 28, 75),
+                      shape: const ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const ReportsScreen(),
+                        ),
+                      );
+                    },
+                    icon: const Icon(Icons.add_chart),
+                    label: const Text('Reports'),
+                  ),
+                  const SizedBox(height: 20),
+                  ElevatedButton.icon(
+                    style: ElevatedButton.styleFrom(
+                      fixedSize: const Size(200, 80),
+                      backgroundColor: const Color.fromARGB(255, 35, 28, 75),
+                      shape: const ContinuousRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(50)),
+                      ),
+                    ),
+                    onPressed: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) => RequestMaintenanceScreen()),
+                      // );
+                    },
+                    icon: const Icon(Icons.add_ic_call),
+                    label: const Text('Request Maintenance'),
+                  ),
+                ],
               ),
-              const SizedBox(height: 20),
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(200, 80),
-                    backgroundColor: const Color.fromARGB(255, 35, 28, 75),
-                    shape: const ContinuousRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)))),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const DataScreen()),
-                  );
-                },
-                icon: const Icon(Icons.account_tree_rounded),
-                label: const Text('Real Time Tracking'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(200, 80),
-                    backgroundColor: const Color.fromARGB(255, 35, 28, 75),
-                    shape: const ContinuousRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)))),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => const ReportsScreen()),
-                  );
-                },
-                icon: const Icon(Icons.add_chart),
-                label: const Text('Reports'),
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton.icon(
-                style: ElevatedButton.styleFrom(
-                    fixedSize: const Size(200, 80),
-                    backgroundColor: const Color.fromARGB(255, 35, 28, 75),
-                    shape: const ContinuousRectangleBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(50)))),
-                onPressed: () {
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(
-                  //       builder: (context) => RequestMaintenanceScreen()),
-                  // );
-                },
-                icon: const Icon(Icons.add_ic_call),
-                label: const Text('Request Maintenance'),
-              ),
-            ],
-          ),
+            ),
+          ],
         ),
       );
     }
   }
 }
+
+
+
+
+
+
+
+
+
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/material.dart';
+
+// import 'addfabric_details.dart';
+// import 'data_display.dart'; // Import your real-time data screen
+// import 'maintenance_screen.dart';
+// import 'reports_screen.dart';
+// import 'signin_screen.dart';
+
+// class HomeScreen extends StatefulWidget {
+//   const HomeScreen({super.key});
+
+//   @override
+//   _HomeScreenState createState() => _HomeScreenState();
+// }
+
+// class _HomeScreenState extends State<HomeScreen> {
+//   final String currentUserUID = FirebaseAuth.instance.currentUser?.uid ?? "";
+//   @override
+//   Widget build(BuildContext context) {
+//     if (currentUserUID == "v7GgJmld33eU5K16QSBF7kgFIK23") {
+//       // If the current user's UID matches, navigate to MaintenanceScreen
+//       return const MaintenanceScreen();
+//     } else {
+//       return Scaffold(
+//         //backgroundColor: Color.fromARGB(255, 18, 19, 26),
+//         appBar: AppBar(
+//           actions: <Widget>[
+//             IconButton(
+//               icon: const Icon(
+//                 Icons.logout,
+//                 color: Colors.white,
+//               ),
+//               onPressed: () {
+//                 FirebaseAuth.instance.signOut().then((value) {
+//                   print("Signed Out");
+//                   Navigator.push(
+//                       context,
+//                       MaterialPageRoute(
+//                           builder: (context) => const SignInScreen()));
+//                 });
+//               },
+//             )
+//           ],
+//           shape: const RoundedRectangleBorder(
+//             borderRadius: BorderRadius.vertical(
+//               bottom: Radius.circular(15),
+//             ),
+//           ),
+//           toolbarHeight: 110,
+//           backgroundColor: const Color.fromARGB(255, 18, 19, 26),
+//           title: Image.asset(
+//             'assets/images/FabriTrack logo.png',
+//             width: 90,
+//             height: 90,
+//           ),
+//         ),
+//         body: Center(
+//           child: Column(
+//             children: [
+//               const SizedBox(height: 150),
+//               ElevatedButton.icon(
+//                 style: ElevatedButton.styleFrom(
+//                     fixedSize: const Size(200, 80),
+//                     backgroundColor: const Color.fromARGB(255, 35, 28, 75),
+//                     shape: const ContinuousRectangleBorder(
+//                         borderRadius: BorderRadius.all(Radius.circular(50)))),
+//                 onPressed: () {
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(
+//                         builder: (context) => const FabricScreen()),
+//                   );
+//                 },
+//                 icon: const Icon(Icons.add),
+//                 label: const Text('Add Fabric Details'),
+//               ),
+//               const SizedBox(height: 20),
+//               ElevatedButton.icon(
+//                 style: ElevatedButton.styleFrom(
+//                     fixedSize: const Size(200, 80),
+//                     backgroundColor: const Color.fromARGB(255, 35, 28, 75),
+//                     shape: const ContinuousRectangleBorder(
+//                         borderRadius: BorderRadius.all(Radius.circular(50)))),
+//                 onPressed: () {
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(builder: (context) => const DataScreen()),
+//                   );
+//                 },
+//                 icon: const Icon(Icons.account_tree_rounded),
+//                 label: const Text('Real Time Tracking'),
+//               ),
+//               const SizedBox(height: 20),
+//               ElevatedButton.icon(
+//                 style: ElevatedButton.styleFrom(
+//                     fixedSize: const Size(200, 80),
+//                     backgroundColor: const Color.fromARGB(255, 35, 28, 75),
+//                     shape: const ContinuousRectangleBorder(
+//                         borderRadius: BorderRadius.all(Radius.circular(50)))),
+//                 onPressed: () {
+//                   Navigator.push(
+//                     context,
+//                     MaterialPageRoute(
+//                         builder: (context) => const ReportsScreen()),
+//                   );
+//                 },
+//                 icon: const Icon(Icons.add_chart),
+//                 label: const Text('Reports'),
+//               ),
+//               const SizedBox(height: 20),
+//               ElevatedButton.icon(
+//                 style: ElevatedButton.styleFrom(
+//                     fixedSize: const Size(200, 80),
+//                     backgroundColor: const Color.fromARGB(255, 35, 28, 75),
+//                     shape: const ContinuousRectangleBorder(
+//                         borderRadius: BorderRadius.all(Radius.circular(50)))),
+//                 onPressed: () {
+//                   // Navigator.push(
+//                   //   context,
+//                   //   MaterialPageRoute(
+//                   //       builder: (context) => RequestMaintenanceScreen()),
+//                   // );
+//                 },
+//                 icon: const Icon(Icons.add_ic_call),
+//                 label: const Text('Request Maintenance'),
+//               ),
+//             ],
+//           ),
+//         ),
+//       );
+//     }
+//   }
+// }
